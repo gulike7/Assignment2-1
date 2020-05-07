793f527bd9e926a0eccb616328155e3c54be27a2
gulike7 <gulike7q@gmail.com>
2020-05-07 07:40:06 -0700

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm time1, time2;
 
 void filestat1();
 void filestat2();
@@ -54,26 +54,12 @@ void filestat2(){
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    time1 = localtime(&stat1.st_mtime);
-    if(time1 == NULL){
-        printf("Get File1 Time Error!\n");
-        exit(0);
-    } 
-    else{
-        return;
-    }
+    time1 = *localtime(&stat1.st_mtime);
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    time2 = localtime(&stat2.st_mtime);
-    if(time2 == NULL){
-        printf("Get File2 Time Error!\n");
-        exit(0);
-    } 
-    else{
-        return;
-    }
+    time2 = *localtime(&stat2.st_mtime);
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
@@ -88,10 +74,34 @@ void blockcmp(){
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(){
-    
+    printf("date compare\n");  
+	if(time1.tm_mon+1>time2.tm_mon+1)
+		printf("text2 is early\n\n");
+	else if(time1.tm_mon+1<time2.tm_mon+1)
+		printf("text1 is early\n\n");
+	else {
+		if(time1.tm_mday>time2.tm_mday)
+			printf("text2 is early\n");
+		else if(time1.tm_mday<time2.tm_mday)
+			printf("text1 is early\n");
+		else
+			printf("same date\n");
+	}
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(){
-    
+    printf("time compare\n");
+	if(time1.tm_hour>time2.tm_hour)
+		printf("text2 is early\n");
+	else if((int)time1.tm_hour < (int)time2.tm_hour)
+		printf("text1 is early\n");    
+	else {
+		if(time1.tm_min>time2.tm_min)
+			printf("text2 is early\n");
+		else if(time1.tm_min<time2.tm_min)
+			printf("text1 is early\n");
+		else
+			printf("same time\n");
+	}
 }
